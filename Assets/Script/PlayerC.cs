2a30ec593c7326320b9e3a9b80106161892918a6
J. Santiago Restrepo <santi1404@hotmail.com>
2022-10-19 12:04:47 -0500

    HUD1

--- Assets/Script/PlayerC.cs
+++ Assets/Script/PlayerC.cs
@@ -8,9 +8,14 @@ public class PlayerC : MonoBehaviour
 {
     Rigidbody2D rb;
     Animator animator;
+    Animator animatorHud;
 
     public float velocidad;
     public float salto;
+
+    public int vida;
+    public int vidaEnemigo;
+    public int daño;
 
     private bool moving;
     private bool moving2;
@@ -20,10 +25,14 @@ public class PlayerC : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();  
         animator = GetComponent<Animator>();
+
+        vida = 3;
+        daño = 1;
     }
 
     void Update()
     {
+        ControlHud();
         Movimiento();
         Ataque();
     }
@@ -57,6 +66,49 @@ public class PlayerC : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             animator.SetTrigger("Attack");
+        }
+    }
+
+    private void ControlHud()
+    {
+        if(vida == 3 && vidaEnemigo == 2)
+        {
+
+        }
+
+        if (vida == 3 && vidaEnemigo == 1)
+        {
+
+        }
+
+        if (vida == 2 && vidaEnemigo == 3)
+        {
+
+        }
+
+        if (vida == 2 && vidaEnemigo == 2)
+        {
+
+        }
+
+        if (vida == 2 && vidaEnemigo == 1)
+        {
+
+        }
+
+        if (vida == 1 && vidaEnemigo == 3)
+        {
+
+        }
+
+        if (vida == 1 && vidaEnemigo == 2)
+        {
+
+        }
+
+        if (vida == 1 && vidaEnemigo == 1)
+        {
+
         }
     }
 
