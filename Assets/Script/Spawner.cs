8125bb047e417ab50967c9731b87a1a8efbc75c4
TATO DESIGN <santi1404@hotmail.com>
2022-10-20 00:23:28 -0500

    HUD #2

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -1,12 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour
 {
+
+    private GameObject rojo;
+    private GameObject azul;
+    private GameObject verde;
+
+    public Image hudA;
+
     void Start()
     {
         Instantiate(Comunicador.playerPrefab, this.transform.position, this.transform.rotation);
-        Destroy(this.gameObject);
+
+        /*rojo = GameObject.FindGameObjectWithTag("Paisa");
+        azul = GameObject.FindGameObjectWithTag("Rolo");
+        verde = GameObject.FindGameObjectWithTag("Caleño");*/
+
+    }
+
+    private void Update()
+    {
+        if(GameObject.FindGameObjectWithTag("Rolo").activeInHierarchy == true)
+        {
+            hudA.gameObject.SetActive(true);
+        }
+
+       /* if (rojo.activeInHierarchy == true)
+        {
+            
+        }
+
+        if (verde.activeInHierarchy == true)
+        {
+            
+        }*/
     }
 }
